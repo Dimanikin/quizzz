9abde11537de1cdef80e541f451509cd42505b60
Dimanikin <driver-s@mail.ru>
2021-01-31 20:04:10 +0200

    Initial Commit

---
+++ Quizzz/Model/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzz
+//
+//  Created by Dima on 1/31/21.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let answer: String
+    
+}
---
+++ Quizzz/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  Quizzz
+//
+//  Created by Dima on 1/31/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+}
+
